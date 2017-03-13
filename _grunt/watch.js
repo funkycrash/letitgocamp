f3405ca2375b6b5e0fa4e09bf5eceaae8fc5ba7d
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-03-13 14:21:32 -0700

    adding workgroups

--- _grunt/watch.js
+++ _grunt/watch.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     jekyll: {
         files: ['_layouts/*.html', '_includes/*.md', '_includes/*.html', 'css/main.css', '*.html', '*.md'],
-        tasks: ['shell:commentContentful', 'shell:jekyllBuild', 'shell:uncommentContentful']
+        tasks: ['shell:interpolateEnvVariables', 'shell:jekyllBuildContentful', 'shell:removeAPIKeys']
     }
 }
 
